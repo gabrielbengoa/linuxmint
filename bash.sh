c9bf3565dc70188fbaaf93491268fa5eb0953b7f
gabrielbengoa <53578022+gabrielbengoa@users.noreply.github.com>
2019-08-06 15:19:42 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,23 @@
 Gabriel Barros Bengoa, SENAC Tatuapé, aprendendo Git e GitHub...
+
+Terminal - CTRL+ALT+F1 (sem ambiente gráfico)
+           CTRL+ALT+F7 (retornar o ambiente gráfico)
+
+
+~-> diretório pessoal
+ls -> lista todos os arquivos e diretórios
+ls -lh -> lista e mostra a data de criação + tamanho dos arquivos
+CTRL+L -> limpa o terminal
+cd / -> manda para a raiz do linux
+cd -> te leva para um diretório
+
+azul escuro -> diretório
+azul claro -> atalho
+cinza -> arquivos
+verde -> perigo
+
+
+cd - -> volta
+pwd -mostra a localização de onde você está
+mkdir -> cria um diretório
+TAB -> autocompleta
